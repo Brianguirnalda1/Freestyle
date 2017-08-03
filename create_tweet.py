f209142c77f58d43d845da12e58506dbd35ca299
Brian Guirnalda <Brian.guirnalda@stern.nyu.edu>
2017-08-02 21:39:41 -0400

    My Freestyle Project

---
+++ create_tweet.py
@@ -0,0 +1,23 @@
+import tweepy
+
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+def main():
+  # Fill in the values noted in previous step here
+  cfg = {
+    "consumer_key"        : "O3yujMTwC0dJehCMfGGpqBxME",
+    "consumer_secret"     : "2g5kxhhhjOLZKGpmR4K2A2uqM2w2zHJ3rCNeQlZ9gcE7rabazj",
+    "access_token"        : "892884097554685952-lzQmDO8WEWmiOgdGuz4XkTYXy6Pp6wQ",
+    "access_token_secret" : "0vZrOY3bKdONbQuafToGAt3eXKOgMg2PMoO3D7nL6PuMO"
+    }
+
+  api = get_api(cfg)
+  tweet = input("Write Text Here")
+  status = api.update_status(status=tweet)
+  # Yes, tweet is called 'status' rather confusing
+
+if __name__ == "__main__":
+  main()
---
+++ twitter_app.py
@@ -0,0 +1,34 @@
+import os
+import tweepy
+import create_tweet
+create_tweet.main()
+
+consumer_key = os.environ["TWITTER_API_KEY"]
+consumer_secret = os.environ["TWITTER_API_SECRET"]
+access_token = os.environ["TWITTER_ACCESS_TOKEN"]
+access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+
+# AUTHENTICATE
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# INITIALIZE API CLIENT
+
+api = tweepy.API(auth)
+
+# ISSUE REQUESTS
+
+user = api.me() # get information about the currently authenticated user
+
+tweets = api.user_timeline() # get a list of tweets posted by the currently authenticated user
+
+# PARSE RESPONSES
+
+print("---------------------------------------------------------------")
+print("RECENT TWEETS BY @{0} ({1} FOLLOWERS / {2} FOLLOWING):".format(user.screen_name, user.followers_count, user.friends_count))
+print("---------------------------------------------------------------")
+
+for tweet in tweets:
+    created_on = tweet.created_at.strftime("%Y-%m-%d")
+    print(" + ", tweet.id_str, created_on, tweet.text)
